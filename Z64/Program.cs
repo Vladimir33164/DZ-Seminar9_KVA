e491e103ddfa007c8e52e9f12a2855f54d0cb2b4
Vladimir <108830774+Vladimir33164@users.noreply.github.com>
2022-09-06 17:37:23 +0300

    напишите программу, которая выводит все натуральные числа от M до N

---
+++ Z64/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 64: Задайте значения M и N. Напишите программу, которая выведет все 
+//натуральные числа в промежутке от M до N.
+
+//M = 1; N = 5. -> "1, 2, 3, 4, 5"
+//M = 4; N = 8. -> "4, 6, 7, 8"
+
+
+
+// ввод данных
+Console.Write("Enter a value M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter a value N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (M > N) // если M больше чем N то выводим ошибку
+    Console.WriteLine("The value of M must be less than or equal to N!");
+else // иначе
+    for (int i = M; i <= N; i++) // в цикле от M до N выводим значение счетчика
+        Console.Write(i + ", ");
